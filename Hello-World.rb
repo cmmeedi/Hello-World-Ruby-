1f6d234370696f9be0a45d6701c24e8f69245b62
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2022-02-02 20:37:04 -0800

    Add files via upload

---
+++ Hello-World.rb
@@ -0,0 +1,7 @@
+#Put will out print on a new line each time it is declared
+puts "Hello World"
+puts "Compa"
+
+#Print will output the text right after the last 'print' statement
+print "Hello World"
+print " Essay"
